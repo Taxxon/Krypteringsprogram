3a4bef662b820d775826e4435b8451570bc6808c
Taxxon <te15.emil.kack@umea.nti.se>
2017-09-20 13:21:07 +0200

    Inte klar men har fixat så man får up något på skärmen.

--- src/GUIDesign.java
+++ src/GUIDesign.java
@@ -1,6 +1,7 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
 
@@ -20,13 +21,14 @@ public class GUIDesign extends Canvas {
 
     public GUIDesign() {
         LoadImageApp();
-        Dimension dim = new Dimension(600, 602);
+        Dimension dim = new Dimension(600, 600);
         setPreferredSize(dim);
         frame = new JFrame("Encryption Program");
         frame.add(this);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        this.repaint();
     }
 
     public void LoadImageApp() {
@@ -44,14 +46,20 @@ public class GUIDesign extends Canvas {
                 System.out.println("Img is still null");
                 return;
             } else {
-                bdg = bild.getGraphics();
+                bdg = img.getGraphics();
             }
             bdg.fillRect(0, 0, (bild.getWidth(null)), bild.getHeight(null));
             draw(bdg);
         }
+        bdg.fillRect(0, 0, (bild.getWidth(null)), bild.getHeight(null));
+        draw(bdg);
     }
 
     public void draw(Graphics g) {
         g.drawImage(bild, 0, 0, null);
+    }
+
+    public void mouse(MouseEvent e){
+
     }
 }
